ae3932f155ee89f7a810b6253d060c9f1de440dd
Etienne Robillard <erob@cpan.org>
2004-12-30 17:48:24 +0000

    Import of EROB/Erob-L4Env-0.01_09 from CPAN.
    gitpan-cpan-distribution: Erob-L4Env
    gitpan-cpan-version:      0.01_09
    gitpan-cpan-path:         EROB/Erob-L4Env-0.01_09.tar.gz
    gitpan-cpan-author:       EROB
    gitpan-cpan-maturity:     developer

--- lib/Erob/L4Env.pm
+++ lib/Erob/L4Env.pm
@@ -2,21 +2,19 @@
 # Erob::L4Env - 
 # Access L4 primitives with L4Env, Dice compiler.
 
-# Yeah, it changed. 
-# The main reason is to avoid/resolve
-# ambiguities and namespace collisions
-# with the real L4Env and Dice authors. -erob
+# Avoid ambiguities and namespaces collisions
+# with L4Env et al. 
 package Erob::L4Env;
 
-# Maintain backward compatibility -- unless
-# we have or need features in perl 5.8 and above...
+# stick with 5.0.5 
+# unless we need features in >= 5.8. 
 use 5.00005;
 
 use vars qw($VERSION);
 
 use strict;
 
-$VERSION = "0.01_07";
+$VERSION = "0.01_09";
 
 # Preloaded methods go here.
 
@@ -27,10 +25,11 @@ sub MkPrivateHeader {
 	# Parameters are:
 	# 	libio			=> Generate code for the GenericIO subsystem.
 	#	libsomething	=> Generate code for subsystem SOMETHING.
-	#   libssl			=> Experimental OpenSSL support.
-	# 	output 			=> "lib_hello_client.h"
+	# 	libssl			=> Experimental OpenSSL support.
+	#	libsdl			=> Experimental SDL support.
+	#	output 			=> "lib_hello_client.h"
 	
-	# your code here...
+	# Your code here...
 }
 
 1;
@@ -54,7 +53,27 @@ Erob::L4Env - Perl extension to the L4Env API
 =head1 DESCRIPTION
 
 This module provides modularity towards several L4 subsystem(s).  
-For an introduction to L4, please refer to L<Erob::L4Env::Story::L4>.
+For an introduction to L4, please refer to L<Erob::L4Env::Introduction>.
+
+=head2 Design decisions and implementation
+
+I will describe here two distinct approaches concerning any 
+eventual implementation. 
+
+For the sake of simplicity, the target interface
+should be known as I<hello_world>. 
+
+=head2 Scenario 1: generating hello_world.h from perl
+
+This is the easiest approach so far. It involve generating
+a C header file magically, then to code by hand the
+main program in C langage (with raw l4_* bindings). 
+
+head2 Scenario 2: learning from mod_perl and beyond
+
+This approach consist of developing a perl-interpreter
+portable on L4. So far, there's no report whatsoever
+that perl has ever been ported to L4. 
 
 =head1 METHODS
 
@@ -66,16 +85,16 @@ This method is exported by default.
 
 =back
 
-=head1 SEE ALSO
+=head1 DOCUMENTATION
 
 =over
 
-=item * L<Erob::L4Env::Story::Introduction>	
+=item * L<Erob::L4Env::Introduction>	
 
 Introduction to L4 from a perl-programmer perspective.
 My goals and some incentives surrounding L4 research and development.
 
-=item * L<Erob::L4Env::GenericIO>
+=item * Erob::L4Env::GenericIO
 
 Perlish implementation of DROPS GenericIO. 
 
@@ -84,7 +103,7 @@ use it instead of the OSKit?
 
 Not yet.
 
-=item * L<Erob::L4Env::Story::Notes>
+=item * Erob::L4Env::Notes
 
 Randomly chosen notes. Probably out-of-date :-)
 
